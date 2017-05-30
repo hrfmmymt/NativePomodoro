6a45044ef4c9debf18060ab8b1af727c1e3f265f
hrfmmymt <hrfmmymt@gmail.com>
2017-05-30 10:14:33 +0900

    init commit

---
+++ app/actions/nav-actions.js
@@ -0,0 +1,55 @@
+import {
+  START_TIMER,
+  PAUSE_TIMER,
+  TICK_TIMER,
+  CLEAR_TIMER,
+  FINISH_TIMER,
+  SET_POMODORO_LENGTH,
+  SET_BREAK_LENGTH,
+  SET_ACTIVITY_TYPE
+} from '../constants/action-types'
+
+export const setPomodoroLength = l => ({type: 'SET_POMODORO_LENGTH', length: l})
+export const setBreakLength = l => ({type: 'SET_BREAK_LENGTH', length: l})
+export const setActivityType = t => ({type: 'SET_ACTIVITY_TYPE', activityType: t})
+
+export const startTimer = t => ({
+  type: 'START_TIMER',
+  timer: {
+    isActive: true,
+    isFinished: false,
+    time: t
+  }
+})
+export const pauseTimer = t => ({
+  type: 'PAUSE_TIMER',
+  timer: {
+    isActive: false,
+    paused: true,
+    time: t
+  }
+})
+export const tickTimer = t => ({
+  type: 'TICK_TIMER',
+  timer: {
+    time: t
+  }
+})
+export const clearTimer = () => ({
+  type: 'CLEAR_TIMER',
+  timer: {
+    time: 0,
+    isFinished: false,
+    isActive: false,
+    paused: false
+  }
+})
+export const finishTimer = () => ({
+  type: 'FINISH_TIMER',
+  timer: {
+    time: 0,
+    isFinished: true,
+    paused: false,
+    isActive: false
+  }
+})
---
+++ app/reducers/index.js
@@ -0,0 +1,80 @@
+import {
+  START_TIMER,
+  PAUSE_TIMER,
+  TICK_TIMER,
+  CLEAR_TIMER,
+  FINISH_TIMER,
+  SET_POMODORO_LENGTH,
+  SET_BREAK_LENGTH,
+  SET_ACTIVITY_TYPE
+} from '../constants/action-types'
+
+export const breakLength = (state = (5 * 60000), action) => {
+  switch (action.type) {
+    case 'SET_BREAK_LENGTH': return action.length
+    default: return state
+  }
+}
+
+export const pomodoroLength = (state = (25 * 60000), action) => {
+  switch (action.type) {
+    case 'SET_POMODORO_LENGTH': return action.length
+    default: return state
+  }
+}
+
+export const activityType = (state = 'p', action) => {
+  switch (action.type) {
+    case 'SET_ACTIVITY_TYPE': return action.activityType
+    default: return state
+  }
+}
+
+const timerInitState = {
+  isActive: false,
+  isFinished: false,
+  paused: false,
+  time: 0
+}
+
+export const timer = (state = timerInitState, action) => {
+  switch (action.type) {
+    case 'START_TIMER':
+      return {
+        isActive: action.timer.isActive,
+        isFinished: action.timer.isFinished,
+        paused: state.paused,
+        time: Number(action.timer.time)
+      }
+    case 'PAUSE_TIMER':
+      return {
+        isActive: action.timer.isActive,
+        paused: action.timer.paused,
+        isFinished: state.isFinished,
+        time: Number(action.timer.time)
+      }
+    case 'TICK_TIMER':
+      return {
+        isActive: state.isActive,
+        paused: state.paused,
+        isFinished: state.isFinished,
+        time: Number(action.timer.time)
+      }
+    case 'CLEAR_TIMER':
+      return {
+        isActive: false,
+        paused: false,
+        isFinished: false,
+        time: 0
+      }
+    case 'FINISH_TIMER':
+      return {
+        isActive: false,
+        paused: false,
+        isFinished: true,
+        time: Number(action.timer.time)
+      }
+    default:
+      return state
+  }
+}
